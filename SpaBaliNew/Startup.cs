63af0e003128a723ed1fc8363c9c3b29cea54ca7
Charno van Sonsbeek <0300349@student.rocvantwente.nl>
2020-11-11 11:44:16 +0100

    Add project files.

---
+++ SpaBaliNew/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpaBaliNew.Startup))]
+namespace SpaBaliNew
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
